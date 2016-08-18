7ccc5c2c0a2abe65168cbaae7c321405b378b1e9
Josh Elser <josh.elser@gmail.com>
2016-08-18 13:12:36 -0400

    Start poking at 4.9.0 changes

--- src/main/java/com/github/joshelser/Main.java
+++ src/main/java/com/github/joshelser/Main.java
@@ -3,47 +3,45 @@ package com.github.joshelser;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.log4j.Logger;
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to you under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 /**
  * 
  */
 public class Main {
+  public static final List<String> JDBC_URLS = Arrays.asList("jdbc:phoenix:hw10447.local:2181:/hbase-secure:jelserkeytab@EXAMPLE.COM:/usr/local/lib/hadoop-2.6.0/etc/secure/keytabs/jelserkeytab.service.keytab",
+      "jdbc:phoenix:hw10447.local:2181:/hbase-secure:jelser2@EXAMPLE.COM:/usr/local/lib/hadoop-2.6.0/etc/secure/keytabs/jelser2.headless.keytab");
   
   /**
    * @param args
    */
   public static void main(String[] args) throws Exception {
-    int numThreads = 4;
+    int numThreads = 16;
+//    UserGroupInformation ugi = UserGroupInformation.getLoginUser();
+//    System.out.println(ugi);
+//    System.out.println(ugi.isFromKeytab());
+//    System.out.println(UserGroupInformation.isLoginKeytabBased());
+//    System.out.println(UserGroupInformation.isLoginTicketBased());
     ExecutorService pool = Executors.newFixedThreadPool(1);
     for (int i = 0; i < numThreads; i++) {
       pool.submit(new PhxRunner());
     }
     pool.shutdown();
     pool.awaitTermination(300, TimeUnit.SECONDS);
+//    System.out.println("*****************************************");
+//    System.out.println("*****************************************");
+//    ugi = UserGroupInformation.getLoginUser();
+//    System.out.println(ugi);
+//    System.out.println(ugi.isFromKeytab());
+//    System.out.println(UserGroupInformation.isLoginKeytabBased());
+//    System.out.println(UserGroupInformation.isLoginTicketBased());
     System.out.println("*****************************************");
     System.out.println("Waiting");
     System.out.println("*****************************************");
@@ -51,10 +49,12 @@ public class Main {
   }
 
   private static class PhxRunner implements Runnable {
+    private static final Random rand = new Random();
     public void run() {
       try {
-        System.out.println("Running thread");
-        Connection cnxn = DriverManager.getConnection("jdbc:phoenix:hw10447.local:2181:/hbase-secure:jelserkeytab@EXAMPLE.COM:/usr/local/lib/hadoop-2.6.0/etc/secure/keytabs/jelserkeytab.service.keytab");
+        String url = JDBC_URLS.get(rand.nextInt(JDBC_URLS.size()));
+        System.out.println("Running thread with " + url);
+        Connection cnxn = DriverManager.getConnection(url);
         Statement stmt = cnxn.createStatement();
         ResultSet results = stmt.executeQuery("SELECT * FROM SYSTEM.CATALOG");
         long numRows = 0;
